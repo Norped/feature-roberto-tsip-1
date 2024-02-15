213733dfdd1d0e12d78def4b45921fb1b6a70309
Roberto Pedroza Jr <totongpedroza@hotmail.com>
2024-02-15 10:54:21 +0800

    Add project structure.

---
+++ urlNavigator/src/test/java/com/urlnavigator/api/UrlNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlnavigator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
